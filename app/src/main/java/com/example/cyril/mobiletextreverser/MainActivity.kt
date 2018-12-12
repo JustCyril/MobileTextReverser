cd774f147585df4e54591c755a6beb7e57f32e29
Cyril <justcyrilmobile@gmail.com>
2018-12-12 11:08:33 +0500

    UPD: btn_OnClick-method. Workable.

--- app/src/main/java/com/example/cyril/mobiletextreverser/MainActivity.kt
+++ app/src/main/java/com/example/cyril/mobiletextreverser/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.cyril.mobiletextreverser
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View; // подключаем класс View для обработки нажатия кнопки
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
-import android.widget.EditText; // подключаем класс EditText
+import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,19 +21,8 @@ class MainActivity : AppCompatActivity() {
         btnReverse = findViewById(R.id.btn_reverse)
         mirroredText = findViewById(R.id.mirrored_text)
 
-        btnReverse?.setOnClickListener(View.OnClickListener { DoReverse() })
+        btnReverse?.setOnClickListener(View.OnClickListener { mirroredText?.setText(enteredText?.text.toString().reversed()) })
 
     }
 
-
-    public fun DoReverse() {
-        var enteredMessage = enteredText?.text.toString()
-        val cycleDuration = (enteredMessage.length/2 - enteredMessage.length%2).toInt()
-
-        for (i in 0..cycleDuration-1) {
-            enteredMessage.replace(enteredMessage[i], enteredMessage[enteredMessage.lastIndex-i])
-        }
-
-        mirroredText?.setText(enteredMessage)
-    }
 }
